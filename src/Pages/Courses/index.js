9f025658a55302fac2a5741b1013e52bd88dd7d3
Komal Ahire <komala18@navgurukul.org>
2023-09-28 17:03:37 +0530

    added pages

---
+++ src/Pages/Courses/index.js
@@ -0,0 +1,16 @@
+
+import { useEffect } from "react";
+import React from "react";
+
+
+function Course() {
+
+  return (
+    <div>
+
+     <h1>All course will render here</h1>
+    </div>
+  );
+}
+
+export default Course;

89de66e7616ab57a1ae74cd0cb4d446b10b42345
Komal Ahire <komala18@navgurukul.org>
2023-09-30 13:02:17 +0530

    added table fields

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,109 +1,90 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { Grid ,Container, Table} from '@mui/material';
-import TextSnippetOutlinedIcon from '@mui/icons-material/TextSnippetOutlined';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Grid, Container, Table } from "@mui/material";
+import TextSnippetOutlinedIcon from "@mui/icons-material/TextSnippetOutlined";
 import LinearProgress from "@mui/material/LinearProgress";
-import { LineChart } from '@mui/x-charts/LineChart';
-import TableProgress from './TableProgress';
+import { LineChart } from "@mui/x-charts/LineChart";
+import TableProgress from "./TableProgress";
+
+
 function Dashboard() {
-  ;
-  
-    return (
-    <Container maxWidth="md">
-    <Grid container spacing={2} mt="16px" mb="32px">
-    <Grid item xs={12} sm={6} md={4}>
-      <Card elevation={3} style={{borderRadius:"8px"}}>
-        <CardContent pt="16px">
-        <div style={{display:"flex",}}>
-          <Typography variant="h6" component="div">
-           No of Students
-          </Typography>
-          <TextSnippetOutlinedIcon style={{marginLeft:"auto", color:"#87CEEB"}}/>
-        </div>
-        <div style={{display:"flex", marginTop:"20px"}}>
-        <Typography variant="h3" style={{color:"red"}}>
-            56 
-          </Typography>
-          <Typography variant='body1' style={{marginTop:"24px"}}>
-          {"  "}
-          Section 1</Typography>
-           
-        </div>
-         
-        </CardContent>
-      </Card>
+  return (
+    <Container maxWidth="lg">
+      <Grid container spacing={2} mt="16px" mb="32px">
+        <Grid item xs={12} sm={6} md={4}>
+          <Card elevation={3} style={{ borderRadius: "8px" }}>
+            <CardContent pt="16px">
+              <div style={{ display: "flex" }}>
+                <Typography variant="h6" component="div">
+                  No of Students
+                </Typography>
+                <TextSnippetOutlinedIcon
+                  style={{ marginLeft: "auto", color: "#87CEEB" }}
+                />
+              </div>
+              <div style={{ display: "flex", marginTop: "20px" }}>
+                <Typography variant="h3" style={{ color: "red" }}>
+                  56
+                </Typography>
+                <Typography variant="body1" style={{ marginTop: "24px" }}>
+                  {"  "}
+                  Section 1
+                </Typography>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card elevation={3} style={{ borderRadius: "8px" }}>
+            <CardContent pt="16px">
+              <div style={{ display: "flex" }}>
+                <Typography variant="h6" component="div">
+                  No of Students
+                </Typography>
+                <TextSnippetOutlinedIcon
+                  style={{ marginLeft: "auto", color: "#87CEEB" }}
+                />
+              </div>
+              <div style={{ display: "flex", marginTop: "20px" }}>
+                <Typography variant="h3" style={{ color: "red" }}>
+                  56
+                </Typography>
+                <Typography variant="body1" style={{ marginTop: "24px" }}>
+                  {"  "}
+                  Section 1
+                </Typography>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card elevation={3} style={{ borderRadius: "8px" }}>
+            <CardContent pt="16px">
+              <div style={{ display: "flex" }}>
+                <Typography variant="h6" component="div">
+                  No of Students
+                </Typography>
+                <TextSnippetOutlinedIcon
+                  style={{ marginLeft: "auto", color: "#87CEEB" }}
+                />
+              </div>
+              <div style={{ display: "flex", marginTop: "20px" }}>
+                <Typography variant="h3" style={{ color: "red" }}>
+                  56
+                </Typography>
+                <Typography variant="body1" style={{ marginTop: "24px" }}>
+                  {"  "}
+                  Section 1
+                </Typography>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-      <Card elevation={3} style={{borderRadius:"8px"}}>
-        <CardContent pt="16px">
-        <div style={{display:"flex",}}>
-          <Typography variant="h6" component="div">
-           No of Students
-          </Typography>
-          <TextSnippetOutlinedIcon style={{marginLeft:"auto", color:"#87CEEB"}}/>
-        </div>
-        <div style={{display:"flex", marginTop:"20px"}}>
-        <Typography variant="h3" style={{color:"red"}}>
-            56 
-          </Typography>
-          <Typography variant='body1' style={{marginTop:"24px"}}>
-          {"  "}
-          Section 1</Typography>
-           
-        </div>
-         
-        </CardContent>
-      </Card>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-      <Card elevation={3} style={{borderRadius:"8px"}}>
-        <CardContent pt="16px">
-        <div style={{display:"flex",}}>
-          <Typography variant="h6" component="div">
-           No of Students
-          </Typography>
-          <TextSnippetOutlinedIcon style={{marginLeft:"auto", color:"#87CEEB"}}/>
-        </div>
-        <div style={{display:"flex", marginTop:"20px"}}>
-        <Typography variant="h3" style={{color:"red"}}>
-            56 
-          </Typography>
-          <Typography variant='body1' style={{marginTop:"24px"}}>
-          {"  "}
-          Section 1</Typography>
-           
-        </div>
-         
-        </CardContent>
-      </Card>
-      </Grid>
-      
-      </Grid>
-      <Grid container>
-        <Grid item md={8}>
-        <Card elevation={3} style={{borderRadius:"8px",}}>
-      <LineChart
-        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-        series={[
-            { curve: "linear", data: [0, 5, 2, 6, 3, 9.3] },
-            { curve: "linear", data: [6, 3, 7, 9.5, 4, 2] },
-        ]}
-
-        width={500}
-        height={300}
-        />
-        </Card>
-      </Grid>
-    </Grid>
-
-      
-  
-
-
-      <TableProgress/>
-      </Container>
-    );
-  }
-  export default Dashboard;
+      <TableProgress />
+    </Container>
+  );
+}
+export default Dashboard;
--- src/components/Dashboard/TableProgress.js
+++ src/components/Dashboard/TableProgress.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Table,
   TableBody,
@@ -8,67 +8,106 @@ import {
   TableRow,
   Paper,
   Container,
-} from '@mui/material';
+} from "@mui/material";
 import CircularProgress, {
-    circularProgressClasses,
-  } from '@mui/material/CircularProgress';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
-import { styled } from '@mui/material/styles';
+  circularProgressClasses,
+} from "@mui/material/CircularProgress";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+import { styled } from "@mui/material/styles";
+
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-    height: 10,
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
     borderRadius: 5,
-    [`&.${linearProgressClasses.colorPrimary}`]: {
-      backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
-    },
-    [`& .${linearProgressClasses.bar}`]: {
-      borderRadius: 5,
-      backgroundColor: theme.palette.mode === 'light' ? '#1A90FF' : '#308FE8',
-    },
-  }));
+    backgroundColor: theme.palette.mode === "light" ? "#1A90FF" : "#308FE8",
+  },
+}));
+
+
 const TableProgress = () => {
   const studentProgressData = [
-    { student: 'John Doe', studentId: '12345', contentProgress: '90%', grades: "76/100", goal:"B+", performance: 'Great' },
-    { student: 'John Doe', studentId: '12345', contentProgress: '90%', grades: "76/100", goal:"B+", performance: 'Great' },
-    { student: 'John Doe', studentId: '12345', contentProgress: '90%', grades: "76/100", goal:"B+", performance: 'Great' },
-    { student: 'John Doe', studentId: '12345', contentProgress: '90%', grades: "76/100", goal:"B+", performance: 'Great' },
-    { student: 'John Doe', studentId: '12345', contentProgress: '90%', grades: "76/100", goal:"B+", performance: 'Great' },
+    {
+      student: "John Doe",
+      studentId: "12345",
+      contentProgress: "90%",
+      grades: "76/100",
+      goal: "B+",
+      performance: "Great",
+    },
+    {
+      student: "John Doe",
+      studentId: "12345",
+      contentProgress: "90%",
+      grades: "76/100",
+      goal: "B+",
+      performance: "Great",
+    },
+    {
+      student: "John Doe",
+      studentId: "12345",
+      contentProgress: "90%",
+      grades: "76/100",
+      goal: "B+",
+      performance: "Great",
+    },
+    {
+      student: "John Doe",
+      studentId: "12345",
+      contentProgress: "90%",
+      grades: "76/100",
+      goal: "B+",
+      performance: "Great",
+    },
+    {
+      student: "John Doe",
+      studentId: "12345",
+      contentProgress: "90%",
+      grades: "76/100",
+      goal: "B+",
+      performance: "Great",
+    },
     // Add more student progress data as needed
   ];
   return (
-    <Container maxWidth="lg" >
-    <TableContainer component={Paper}>
-      <Table aria-label="Semester Progress">
-        <TableHead>
-          <TableRow>
-            <TableCell>Student</TableCell>
-            <TableCell>Student ID</TableCell>
-            <TableCell>Content Progress</TableCell>
-            <TableCell>Grades</TableCell>
-            <TableCell>Goal</TableCell>
-            <TableCell>Performance</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {studentProgressData.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell>{row.student}</TableCell>
-              <TableCell>{row.studentId}</TableCell>
-              <TableCell><BorderLinearProgress variant="determinate" value={50} /> <br/>
-              {"visited"+row.grades}</TableCell>
-              <TableCell>{row.grades}</TableCell>
-              <TableCell>{row.goal}</TableCell>
-              <TableCell>{row.performance}</TableCell>
+    <Container maxWidth="lg">
+      <TableContainer component={Paper}>
+        <Table aria-label="Semester Progress">
+          <TableHead>
+            <TableRow>
+              <TableCell>Student Name</TableCell>
+              <TableCell>Student Email</TableCell>
+              <TableCell>Time Spent</TableCell>
+              <TableCell>Page viewed</TableCell>
+              <TableCell>Browser</TableCell>
+              <TableCell>OS</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {studentProgressData.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell>{row.student}</TableCell>
+                <TableCell>{row.studentId}</TableCell>
+                <TableCell>
+                  <BorderLinearProgress variant="determinate" value={50} />{" "}
+                  <br />
+                  {"visited" + row.grades}
+                </TableCell>
+                <TableCell>{row.grades}</TableCell>
+                <TableCell>{row.goal}</TableCell>
+                <TableCell>{row.performance}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
 export default TableProgress;
-
-
-
-
-
